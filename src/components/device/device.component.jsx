babec4d0da5da1f3a97aea4c4ef11d0bd1bd6151
Weike Shi <weikeshih@gmail.com>
2022-04-16 23:36:55 -0400

    Update device component style

--- src/components/device/device.component.jsx
+++ src/components/device/device.component.jsx
@@ -1,6 +1,7 @@
 import { FaCheckCircle } from "react-icons/fa";
 
 const Device = ({ device }) => {
+  const handleOnClick = () => {};
   return (
     <div
       className={`flex gap-x-5 items-center justify-start p-3 rounded-lg h-16 ${
@@ -9,15 +10,18 @@ const Device = ({ device }) => {
     >
       <div className={"w-28 truncate"}>{device.name}</div>
       {device.activate ? (
-        <div className="border-2 border-green-500 p-2 rounded-md text-green-500 flex flex-grow justify-center items-center">
+        <div className="flex flex-grow justify-center items-center border-2 border-green-500 p-2 rounded-md text-green-500 hover:text-white hover:bg-green-500 transition duration-200">
           <FaCheckCircle />
         </div>
       ) : (
         <>
-          <button className="font-light border-2 border-blue-500 p-2 rounded-md hover:bg-blue-500 hover:text-white">
+          <button
+            className="font-light border-2 border-blue-500 p-2 rounded-md hover:bg-blue-500 hover:text-white transition duration-200"
+            onClick={handleOnClick}
+          >
             Activate
           </button>
-          <button className="font-light border-2 border-red-500 p-2 rounded-md hover:bg-red-500 hover:text-white">
+          <button className="font-light border-2 border-red-500 p-2 rounded-md hover:bg-red-500 hover:text-white transition duration-200">
             Remove
           </button>
         </>
