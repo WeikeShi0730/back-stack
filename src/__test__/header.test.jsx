290bbe0a6cb84e51ad4db4a25e3ac68671d4c8dd
Weike Shi <weikeshih@gmail.com>
2022-04-26 23:58:52 -0400

    Update test for <Header />

--- src/__test__/header.test.jsx
+++ src/__test__/header.test.jsx
@@ -1,10 +1,13 @@
-import { render } from "@testing-library/react";
+import { render, renderer } from "@testing-library/react";
+import { BrowserRouter } from "react-router-dom";
 import Header from "../components/header/header.component";
 
 describe("<Header />", () => {
   test("<Header />", () => {
     const { baseElement } = render(
+      <BrowserRouter>
         <Header />
+      </BrowserRouter>
     );
     expect(baseElement).toMatchSnapshot();
   });
