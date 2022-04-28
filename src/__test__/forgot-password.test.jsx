b168132b49299543334c344db8cd8a94138704f2
Weike Shi <weikeshih@gmail.com>
2022-04-27 21:38:40 -0400

    Add test for <SignIn />

--- src/__test__/forgot-password.test.jsx
+++ src/__test__/forgot-password.test.jsx
@@ -1,9 +1,14 @@
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import ForgotPassword from "../components/forgot-password/forgot-password.component";
 
+let documentBody;
 describe("<ForgotPassword />", () => {
   test("<ForgotPassword />", () => {
-    const { baseElement } = render(<ForgotPassword />);
+    documentBody = render(<ForgotPassword />);
+    expect(
+      screen.getByRole("button", { name: /Send a password reset email/i })
+    ).toBeInTheDocument();
+    const { baseElement } = documentBody;
     expect(baseElement).toMatchSnapshot();
   });
 });
--- src/__test__/sign-in.test.jsx
+++ src/__test__/sign-in.test.jsx
@@ -1,18 +1,36 @@
-import { render, screen } from "@testing-library/react";
-// import { Router } from "react-router-dom";
-// import { createMemoryHistory } from "history";
+import { render, fireEvent, screen } from "@testing-library/react";
+import { Router } from "react-router-dom";
+import { createMemoryHistory } from "history";
 import SignIn from "../components/sign-in/sign-in.component";
+import createMockRouter from "./test-utils/createMockRouter";
 
 let documentBody;
+const mockHistoryPush = jest.fn();
+jest.mock("react-router-dom", () => ({
+  ...jest.requireActual("react-router-dom"),
+  useHistory: () => ({
+    push: mockHistoryPush,
+  }),
+}));
 describe("<SignIn />", () => {
-//   const history = createMemoryHistory();
+  const history = createMemoryHistory();
   test("<SignIn />", () => {
     documentBody = render(<SignIn />);
     expect(
       screen.getByText("Alrady have an account? Sign in 🔐")
     ).toBeInTheDocument();
     expect(screen.getAllByRole("button", { name: /Sign in/i })).toHaveLength(2);
+
     const { baseElement } = documentBody;
     expect(baseElement).toMatchSnapshot();
+  });
+  test("click router <SignIn />", () => {
+    documentBody = render(
+      <Router history={history}>
+        <SignIn />
+      </Router>
+    );
+    fireEvent.click(screen.getByRole("button", { name: /Forgot password?/i }));
+    expect(mockHistoryPush).toHaveBeenCalledWith("/reset-password");
   });
 });
