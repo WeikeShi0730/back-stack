fa38ab53c203f49f6cf89051de7ebb0cac1f3e67
Weike Shi <weikeshih@gmail.com>
2022-04-29 23:50:55 -0400

    Add screen size for <Header />

--- src/__test__/header.test.jsx
+++ src/__test__/header.test.jsx
@@ -1,14 +1,17 @@
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import { BrowserRouter } from "react-router-dom";
 import Header from "../components/header/header.component";
 
 describe("<Header />", () => {
   test("<Header />", () => {
+    window.innerWidth = 2400;
+    window.dispatchEvent(new Event("resize"));
     const { baseElement } = render(
       <BrowserRouter>
         <Header />
       </BrowserRouter>
     );
+    expect(screen.getByText("Home 🏠")).toBeInTheDocument();
     expect(baseElement).toMatchSnapshot();
   });
 });
